46d07eb3e47a93eba66fd951796e436016d92f87
jamera mcneil <j.mcneil04@gmail.com>
2021-02-07 21:11:56 -0500

    added a modal to share instructions and details about the game

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 console.log('is this thing on?')
 
 $(() => {
-    
+    // setting up modal //
+
+     //Grabbing About the Game button
+  const $openBtn = $("#openModal");
+  
+  // Grabbing modal element
+  const $modal = $("#modal");
+
+  //Grabbing close button
+  const $closeBtn = $("#close");
+
+      // Event Handlers //
+
+  // Event handler to open the modal
+  const openModal = () => {
+    $modal.css("display", "block");
+    setTimeout(openModal, 5000);
+  }
+
+  // Add event handler to close the button
+
+  const closeModal = () => {
+    $closeBtn.css("click", "none");
+  }
+
+    // Event Listeners //
+
+  // Add event listerner on the About Game Button
+  $openBtn.on("click", openModal);
+
+  // Add event listener to Close button
+  $closeBtn.on("click", closeModal);
 })
