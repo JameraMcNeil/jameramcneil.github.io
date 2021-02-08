c68589ae105942c9321378d68b821d169a34428f
jamera mcneil <j.mcneil04@gmail.com>
2021-02-08 14:29:09 -0500

    added readme.md file and updated fixed modal functionality

--- app.js
+++ app.js
@@ -4,33 +4,32 @@ $(() => {
     // setting up modal //
 
      //Grabbing About the Game button
-  const $openBtn = $("#openModal");
+  const $openBtn = $('#openModal');
   
   // Grabbing modal element
-  const $modal = $("#modal");
+  const $modal = $('#modal');
 
   //Grabbing close button
-  const $closeBtn = $("#close");
+  const $closeBtn = $('#close');
 
       // Event Handlers //
 
   // Event handler to open the modal
   const openModal = () => {
-    $modal.css("display", "block");
-    setTimeout(openModal, 5000);
+    $modal.css('display', 'block');
   }
 
   // Add event handler to close the button
 
   const closeModal = () => {
-    $closeBtn.css("click", "none");
+    $modal.css('display', 'none');
   }
 
     // Event Listeners //
 
-  // Add event listerner on the About Game Button
-  $openBtn.on("click", openModal);
+  // Add event listener on the About Game Button
+  $openBtn.on('click', openModal);
 
   // Add event listener to Close button
-  $closeBtn.on("click", closeModal);
+  $closeBtn.on('click', closeModal);
 })
