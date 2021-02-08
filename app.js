45bcbe8dec7bd48520b48e8e21fe0d6a82c5ae49
jamera mcneil <j.mcneil04@gmail.com>
2021-02-08 15:08:57 -0500

    added on click event listeners to js file and styling in css

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 console.log('is this thing on?')
 
 $(() => {
-    // setting up modal //
+
+    // As a user, I am able to select and deselect a modal to learn the directions of the game //
+    // setting up modal 
 
      //Grabbing About the Game button
   const $openBtn = $('#openModal');
@@ -32,4 +34,29 @@ $(() => {
 
   // Add event listener to Close button
   $closeBtn.on('click', closeModal);
+
+  // Setting Up Game //
+
+  // As a user, I am able to choose an $dollar amount from any category ranging from $100-$500 under each category
+
+  // Grab each $$$ category and create a new jQuery variable
+  const $films100 = $('.films-100')
+  const $films200 = $('.films-200')
+  const $films300 = $('.films-300')
+  const $films400 = $('.films-400')
+  const $films500 = $('.films-500')
+
+  // Add event handlers to films category
+
+  const turnPink = () => {
+      $films100.css('background-color', '#FF91D2');
+      $films100.css('color', 'black')
+  }
+
+  // Add event listeners to films category
+  $films100.on('click', turnPink)
+
+  // Add a class specifically for that category
+  // Add the text that will be displayed in that category
+  // Append the class of the specified category to it's parent class
 })
